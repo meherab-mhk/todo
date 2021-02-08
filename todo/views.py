b7eaded9d2cfae0034196ef54fb003cfa5b3a33b
meherab-mhk <meherabh43@gmail.com>
2021-02-08 17:58:05 +0600

    todo app create and modify

---
+++ todo/views.py
@@ -0,0 +1,21 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Todo
+
+# Create your views here.
+def todo_list(request):
+    todos = Todo.objects.all()
+    print(todos)
+    context = {
+        "todo_list": todos
+    }
+    return render(request,"todo/todo_list.html", context)
+
+#CRUD , Create , retrieve , update , delete , delete
+
+def todo_detail(request, id):
+    todo = Todo.objects.get(id=id)
+    context = {
+        "todo": todo
+    }
+    return render(request,"todo/todo_detail.html", context)
